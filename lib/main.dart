39914151c9dcd759f74e611e0c5c083c6128a0e6
Charlie <charliedeveloperaccess@gmail.com>
2019-11-07 17:04:48 -0600

    add a richtech editor dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:openote/pages/editor_page.dart';
 import 'package:openote/pages/landing_page.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LandingPage()
+      home: EditorPage()
     );
   }
 }
